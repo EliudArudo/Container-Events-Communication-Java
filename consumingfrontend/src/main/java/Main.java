60d8953ee9294fd54bc8cc7356326888a1aab43d
EliudArudo <eliudarudo@gmail.com>
2020-03-25 20:00:28 +0300

    Testing github actions v4

--- consumingfrontend/src/main/java/Main.java
+++ consumingfrontend/src/main/java/Main.java
@@ -2,7 +2,7 @@ import env.EnvSetup;
 import initialize.*;
 
 public class Main {
-    // Github actions v3
+    // Github actions v4
     public static void main(String[] args) {
         EnvSetup.fetchEnvVariables();
 
